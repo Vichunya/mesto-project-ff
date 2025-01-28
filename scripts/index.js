d09a76efdf41835cb8e63e3f88d56c4f0fc14c07
Vik <victoria.shin.ap@gmail.com>
2025-01-28 19:54:43 +0300

    lesson 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,45 @@
 // @todo: Темплейт карточки
 
-//const cards = document.getElementById('card-template');
-//const cardsCopy = cards.cloneNode(true);
-//const container = document.querySelector();//куда вставить скопированное? 
-   // .append(cardsCopy);
+const cardTemplate = document.getElementById('card-template');
+console.log(1);
+console.log(cardTemplate);
+
+const content = cardTemplate.content; 
+
+
+
+for (let i = 0; i < initialCards.length; i += 1) {
+    console.log(initialCards[i].name);
+    
+    const cardCopy = content.cloneNode(true); 
+    cardCopy.querySelector('.card__image').src = initialCards[i].link;
+    cardCopy.querySelector('.card__image').alt = initialCards[i].name; 
+    const cardList = document.querySelector('.places__list'); 
+    console.log(2);
+    console.log(cardList);
+    cardList.append(cardCopy);
+    
+}
+
+// Этот цикл работает аналогично методу forEach выше
 
 
 // @todo: DOM узлы
 
-const cards = document.querySelector('#card-template').content;
-const cardsItems = document.querySelector('.places__item'); 
-
-const cardsCopy = cards.querySelector('.places__item').cloneNode(true);
 
 // вставляем ссылку и название 
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Архыз'; 
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Челябинская область';
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Иваново';
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Камчатка';
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Холмогорский район';
-cardsCopy.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
-cardsCopy.querySelector('.card__image').alt = 'Байкал';
+
 
   //(что тут должно).append(cardsCopy); .places__list cюда ? 
 
 // @todo: Функция создания карточки
-const button = document.querySelector('button'); 
-button.addEventListener('click', function(){
 
-});
 
 // @todo: Функция удаления карточки
+
+const button = document.querySelector('button'); 
+button.addEventListener('click', function(){
+console.log('click');
+});
 
 // @todo: Вывести карточки на страницу
