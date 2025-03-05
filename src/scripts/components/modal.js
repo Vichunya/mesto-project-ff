2f05abecf93379c3a47fa49d79466bf888d6d22c
Vik <victoria.shin.ap@gmail.com>
2025-03-05 20:00:13 +0300

    modal delete

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -6,28 +6,28 @@ export function openPopup(modal) {
 
 function closePopupEsc(event) {   //удаление разработчика по Esc
     if (event.key === 'Escape') {          // встроенное св-во объекта, какая клавиша нажата
-        const modal = document.querySelector('.popup_is-opened');
-        modal.classList.remove('popup_is-opened');
-        document.removeEventListener('keydown', closePopupEsc);
+        closePopup();
     }
 }
 
 // Закрытие модального окна 
-export function closePopup(modal) {
+export function closePopup() {
+    const modal = document.querySelector('.popup_is-opened');
     modal.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closePopupEsc);
 }
 
 const modals = document.querySelectorAll('.popup'); //закрытие вне окна
 modals.forEach(modal => {
     window.addEventListener('click', function (event) {
         if (event.target === modal) {
-            closePopup(modal);
+            closePopup();
         }
     });
 
     const closeBtn = modal.querySelector('.popup__close'); //закрытие по кнопке
     closeBtn.addEventListener('click', function () {
-        closePopup(modal);
+        closePopup();
     });
 });
 
